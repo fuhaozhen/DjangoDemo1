287ac44daa93ae520b4935da131a90852a6b2a74
fuhaozhen <1542242578@qq.com>
2019-04-16 15:57:22 +0800

    添加超级管理员

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -1,3 +1,10 @@
+"""
+通过少量的代码实现强大的后台管理
+需要将特定的数据模型注册 才能在后台管理
+"""
 from django.contrib import admin
+from .models import BookInfo, HeroInfo
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
 
 # Register your models here.
