615e03b22d78aace513c686660c8f6e6c9c70213
fuhaozhen <1542242578@qq.com>
2019-05-09 11:52:12 +0800

    汽车租赁

--- demo2/cars/views.py
+++ demo2/cars/views.py
@@ -400,49 +400,52 @@ def checkuser(request):
 def verifycode(request):
     # 生成验证码图片
     # 定义变量，用于画面的背景色、宽、高
-    bgcolor = (random.randrange(20, 100),
-               random.randrange(20, 100),
-               random.randrange(20, 100))
-    width = 105
-    heigth = 50
-    # 创建画面对象
-    im = Image.new('RGB', (width, heigth), bgcolor)
-    # 创建画笔对象
-    draw = ImageDraw.Draw(im)
-    # 调用画笔的point()函数绘制噪点
-    for i in range(0, 100):
-        xy = (random.randrange(0, width), random.randrange(0, heigth))
-    fill = (random.randrange(0, 255), 255, random.randrange(0, 255))
-    draw.point(xy, fill=fill)
-    # 定义验证码的备选值
-    str1 = 'ABCD123EFGHIJK456LMNOPQRS789TUVWXYZ0'
-    # 随机选取4个值作为验证码
-    rand_str = ''
-    for i in range(0, 4):
-        rand_str += str1[random.randrange(0, len(str1))]
-    # 构造字体对象
-    # font = ImageFont.truetype('Lobster-Regular.ttf', 24)
-    # fontcolor = (255, random.randrange(0, 255), random.randrange(0, 255))
-    # # 绘制4个字
-    # draw.text((7, 12), rand_str[0], font=font, fill=fontcolor)
-    # draw.text((27, 12), rand_str[1], font=font, fill=fontcolor)
-    # draw.text((52, 12), rand_str[2], font=font, fill=fontcolor)
-    # draw.text((77, 12), rand_str[3], font=font, fill=fontcolor)
+    try:
+        bgcolor = (random.randrange(20, 100),
+                   random.randrange(20, 100),
+                   random.randrange(20, 100))
+        width = 105
+        heigth = 50
+        # 创建画面对象
+        im = Image.new('RGB', (width, heigth), bgcolor)
+        # 创建画笔对象
+        draw = ImageDraw.Draw(im)
+        # 调用画笔的point()函数绘制噪点
+        for i in range(0, 100):
+            xy = (random.randrange(0, width), random.randrange(0, heigth))
+        fill = (random.randrange(0, 255), 255, random.randrange(0, 255))
+        draw.point(xy, fill=fill)
+        # 定义验证码的备选值
+        str1 = 'ABCD123EFGHIJK456LMNOPQRS789TUVWXYZ0'
+        # 随机选取4个值作为验证码
+        rand_str = ''
+        for i in range(0, 4):
+            rand_str += str1[random.randrange(0, len(str1))]
+        # 构造字体对象
+        # font = ImageFont.truetype('Lobster-Regular.ttf', 24)
+        # fontcolor = (255, random.randrange(0, 255), random.randrange(0, 255))
+        # # 绘制4个字
+        # draw.text((7, 12), rand_str[0], font=font, fill=fontcolor)
+        # draw.text((27, 12), rand_str[1], font=font, fill=fontcolor)
+        # draw.text((52, 12), rand_str[2], font=font, fill=fontcolor)
+        # draw.text((77, 12), rand_str[3], font=font, fill=fontcolor)
 
-    draw.text((7, 12), rand_str[0], )
-    draw.text((27, 12), rand_str[1], )
-    draw.text((52, 12), rand_str[2], )
-    draw.text((77, 12), rand_str[3], )
-    # 释放画笔
-    del draw
+        draw.text((7, 12), rand_str[0], )
+        draw.text((27, 12), rand_str[1], )
+        draw.text((52, 12), rand_str[2], )
+        draw.text((77, 12), rand_str[3], )
+        # 释放画笔
+        del draw
 
-    # 将生成的验证码存入session
-    request.session['verifycode'] = rand_str.lower()
+        # 将生成的验证码存入session
+        request.session['verifycode'] = rand_str.lower()
 
-    f = io.BytesIO()
-    im.save(f, 'png')
-    # 将内存中的图片数据返回给客户端，MIME类型为图片png
-    return HttpResponse(f.getvalue(), 'image/png')
+        f = io.BytesIO()
+        im.save(f, 'png')
+        # 将内存中的图片数据返回给客户端，MIME类型为图片png
+        return HttpResponse(f.getvalue(), 'image/png')
+    except Exception as e:
+        return HttpResponse(e)
 
 
 def ajaxload(request):
